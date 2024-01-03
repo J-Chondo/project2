829d796b3e7ecf6aa867fb392edcac9ab67c9f9b
CHONDO DIO <you@example.com>
2024-01-03 08:19:10 +0300

    updating service section

--- index.js
+++ index.js
@@ -40,3 +40,23 @@ window.onload = () => {
       navList.classList.remove("active");
     })
   );
+
+  var swiper = new Swiper(".home-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+  
