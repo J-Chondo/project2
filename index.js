1634a40134b40521286ce28fdd07280db8f82c5f
CHONDO DIO <you@example.com>
2024-02-26 13:43:39 +0300

    finilizing

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
-window.onload = () => {
-    const anchors = document.querySelectorAll('a');
-    const transition_el = document.querySelector('.transition');
-  
-    setTimeout(() => {
-      transition_el.classList.remove('is-active');
-    }, 500);
-  
-    for (let i = 0; i < anchors.length; i++) {
-      const anchor = anchors[i];
-  
-      anchor.addEventListener('click', e => {
-        e.preventDefault();
-        let target = e.target.href;
-  
-        console.log(transition_el);
-  
-        transition_el.classList.add('is-active');
-  
-        console.log(transition_el);
-  
-        setInterval(() => {
-          window.location.href = target;
-        }, 500);
-      })
-    }
-  }
+
 
   const hamburger = document.querySelector(".hamburger");
   const navList = document.querySelector("#nav-list");
@@ -40,5 +14,20 @@ window.onload = () => {
       navList.classList.remove("active");
     })
   );
+
+  const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) =>{
+      console.log(entry)
+      if(entry.isIntersecting) {
+        entry.target.classList.add('show')
+      } else {
+        entry.target.classList.remove('show')
+      }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
 
   
